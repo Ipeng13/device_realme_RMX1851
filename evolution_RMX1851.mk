2173d2d4c55b0a67a8637370fa6d1ab29d60adb3
Hansraj76 <gamerx8x6x@gmail.com>
2022-12-11 15:16:50 +0530

    RMX1851: Evolution of RMX1851

--- evolution_RMX1851.mk
+++ evolution_RMX1851.mk
@@ -6,18 +6,17 @@
 
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := evolution_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 
-# disable/enable blur support, default is false
-TARGET_ENABLE_BLUR := true
+
 
 # Quick tap feature
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -25,10 +24,12 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TD1A.220804.031/9071314:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TD1A.221105.001/9104446:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.220804.031 9071314 release-keys" \
+    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.221105.001 9104446 release-keys" \
     PRODUCT_NAME="RMX1851"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+TARGET_USES_MINI_GAPPS:=true
